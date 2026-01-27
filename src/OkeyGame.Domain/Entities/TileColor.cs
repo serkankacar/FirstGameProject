822ccef37987a5cb50f58e4f59f604db1b0ad52d
Serkan Kaçar <serkankacar@hotmail.com.tr>
2026-01-27 09:05:26 +0300

    feat: Tile renk enum'u eklendi

---
+++ src/OkeyGame.Domain/Entities/TileColor.cs
@@ -0,0 +1,20 @@
+namespace OkeyGame.Domain.Entities;
+
+/// <summary>
+/// Okey taşlarının renklerini temsil eden enum.
+/// Okey oyununda 4 farklı renk bulunur.
+/// </summary>
+public enum TileColor
+{
+    /// <summary>Sarı renk</summary>
+    Yellow = 0,
+    
+    /// <summary>Mavi renk</summary>
+    Blue = 1,
+    
+    /// <summary>Siyah renk</summary>
+    Black = 2,
+    
+    /// <summary>Kırmızı renk</summary>
+    Red = 3
+}
